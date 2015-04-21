95494c50b827146bce4ac0e9ae90c39c0bf53e38
Joshua Zenn <wordman05@gmail.com>
2015-04-21 17:45:01 -0400

    Update HiveStart.rb

--- HiveStart.rb
+++ HiveStart.rb
@@ -5,7 +5,7 @@ puts "This program is distributed under the MIT license"
 puts "Commercial use with this copy of this program is prohibited"
 puts "Starting up..."
 if File.exist?('settings.txt') != true
-  setup
+  #setup
 end
 puts "Getting dependencies"
 require "interpret.rb"
@@ -47,17 +47,17 @@ exit
 #  end  
 #end
 
-def setup()
-  print "Enter a unique hostname for this node: "
-  setup_hostname = gets.strip
-  puts "License"
-  puts "========"
-  puts "By using this program, you agree to the terms listed in LICENSE in the program directory. Since this is the free version, you also agree that this copy of the program MAY NOT be used for commercial or for-profit purposes."
-  print "Accept terms? [y/n]: "
-  license = gets.strip
-  if license != "y" and license != "Y"
-    puts "Did not accept license, terminating program"
-    exit
-  end
-  puts "Finished setup, restarting startup sequence..."
-end
+#def setup()
+#  print "Enter a unique hostname for this node: "
+#  setup_hostname = gets.strip
+#  puts "License"
+#  puts "========"
+#  puts "By using this program, you agree to the terms listed in LICENSE in the program directory. Since this is the free version, you also agree that this copy of the program MAY NOT be used for commercial or for-profit purposes."
+#  print "Accept terms? [y/n]: "
+#  license = gets.strip
+#  if license != "y" and license != "Y"
+#    puts "Did not accept license, terminating program"
+#    exit
+#  end
+#  puts "Finished setup, restarting startup sequence..."
+#end
