9c786e6cdc928430db0759977668ab9a61b4e8a9
Joshua Zenn <wordman05@gmail.com>
2015-04-21 15:47:21 -0400

    Update interpret.rb

--- interpret.rb
+++ interpret.rb
@@ -2,6 +2,12 @@ if __FILE__ = $0
   exit
 end
 
-def HIVE_interpret(cmd)
-
+def HIVE_interpret(cmd, isLocal)
+  if cmd.start_with?("print")
+    if isLocal
+      puts cmd.slice! "print"
+    end
+  else
+    return "PARSEERROR"
+  end
 end
