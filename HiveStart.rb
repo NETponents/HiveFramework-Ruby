9dbb3d9262728fd9cc8aa1604b3fd760b68af348
Joshua Zenn <wordman05@gmail.com>
2015-04-21 12:12:36 -0400

    Update HiveStart.rb

--- HiveStart.rb
+++ HiveStart.rb
@@ -1,4 +1,23 @@
+#!/usr/bin/ruby
+
 puts "HIVE Framework copyright 2015 Joshua Zenn"
 puts "This program is distributed under the MIT license"
-puts "Commercial use with this program is prohibited"
+puts "Commercial use with this copy of this program is prohibited"
 puts "Starting up..."
+if File.exist?('settings.txt') != true
+  setup()
+end
+
+def setup()
+  print "Enter a unique hostname for this node: "
+  setup_hostname = gets.strip
+  puts "License"
+  puts "========"
+  puts "By using this program, you agree to the terms listed in LICENSE in the program directory. Since this is the free version, you also agree that this copy of the program MAY NOT be used for commercial or for-profit purposes."
+  print "Accept terms? [y/n]: "
+  license = gets.strip
+  if license != "y" or "Y"
+    puts "Did not accept license, terminating program"
+    #terminate program
+  end
+end
