32f0f21238d4213d92083f62f13bee21c2c4189e
Joshua Zenn <wordman05@gmail.com>
2015-04-21 11:58:31 -0400

    Create HiveStart.rb

---
+++ HiveStart.rb
@@ -0,0 +1,4 @@
+puts "HIVE Framework copyright 2015 Joshua Zenn"
+puts "This program is distributed under the MIT license"
+puts "Commercial use with this program is prohibited"
+puts "Starting up..."
