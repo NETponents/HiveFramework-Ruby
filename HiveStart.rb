01cb15c6ee115724036ca4784aa218e3a16a2563
Joshua Zenn <wordman05@gmail.com>
2015-04-22 09:22:38 -0400

    Update HiveStart.rb
    Added silent mode

--- HiveStart.rb
+++ HiveStart.rb
@@ -18,20 +18,32 @@ end
 puts "HIVE Framework copyright 2015 Joshua Zenn"
 puts "This program is distributed under the MIT license"
 puts "Commercial use with this copy of this program is prohibited"
+puts "Parsing parameters"
+settings_silent = false
+ARGV.each do|a|
+  if a == "-silent" or a == "-s"
+    settings_silent = true
+  end
+end
 puts "Starting up..."
 if File.exist?('settings.txt') != true
-  print "Enter a unique hostname for this node: "
-  setup_hostname = gets.strip
-  puts "License"
-  puts "========"
-  puts "By using this program, you agree to the terms listed in LICENSE in the program directory. Since this is the free version, you also agree that this copy of the program MAY NOT be used for commercial or for-profit purposes."
-  print "Accept terms? [y/n]: "
-  license = gets.strip
-  if license != "y" and license != "Y"
-    puts "Did not accept license, terminating program"
-    exit
+  if settings_silent
+    puts "Silent mode is on, using default parameters"
+    setup_hostname = "H1N001"
+  else
+    print "Enter a unique hostname for this node: "
+    setup_hostname = gets.strip
+    puts "License"
+    puts "========"
+    puts "By using this program, you agree to the terms listed in LICENSE in the program directory. Since this is the free version, you also agree that this copy of the program MAY NOT be used for commercial or for-profit purposes."
+    print "Accept terms? [y/n]: "
+    license = gets.strip
+    if license != "y" and license != "Y"
+      puts "Did not accept license, terminating program"
+      exit
+    end
   end
-  puts "Finished setup, restarting startup sequence..."
+    puts "Finished setup, restarting startup sequence..."
 end
 puts "Getting dependencies"
 require "./interpret.rb"
