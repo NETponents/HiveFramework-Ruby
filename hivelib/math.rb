f6f00489e63811d349c8115063f03b371eb0c503
Joshua Zenn <wordman05@gmail.com>
2015-04-22 09:43:58 -0400

    Update math.rb

--- hivelib/math.rb
+++ hivelib/math.rb
@@ -1,3 +1,12 @@
 def add(one, two)
   return one + two
 end
+def subtract(one, two)
+  return one - two
+end
+def multiply(one, two)
+  return one * two
+end
+def divide(one, two)
+  return one / two
+end
