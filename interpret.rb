68834bac167ea6994fb5634acc828b503fa0226e
Joshua Zenn <wordman05@gmail.com>
2015-04-24 09:44:08 -0400

    Update interpret.rb

--- interpret.rb
+++ interpret.rb
@@ -28,12 +28,13 @@ def HIVE_interpret(cmd, isLocal, interactive, varStore)
   elsif cmd.start_with?("#")
     #This is a directive
     dcmd = cmd.split(' ')[0]
+    dcmd2 = cmd.split(' ')[1]
     dcmd.slice!("#")
     if dcmd == "HIVE"
       #For now, just print version
       dcmd.slice!('HIVE:')
-      if dcmd.split('.')[3].to_i > varStore["var_hiveenv_version"].split('.')[3].to_i
-        HIVE_print("Insufficient version. You have version #{ var_hiveenv_version }, this program requires #{ dcmd }")
+      if dcmd2.split('.')[3].to_i > varStore["var_hiveenv_version"].split('.')[3].to_i
+        HIVE_print("Insufficient version. You have version #{ var_hiveenv_version }, this program requires #{ dcmd2 }")
         #terminate the program
       else
         HIVE_print("This program requires HIVE v" + cmd.split(' ')[1].to_s)
