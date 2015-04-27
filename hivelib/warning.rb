f9c6f340f85d4dba02060de420a740b862e6aaf0
Joshua Zenn <wordman05@gmail.com>
2015-04-27 13:22:53 -0400

    Create warning.rb

---
+++ hivelib/warning.rb
@@ -0,0 +1,9 @@
+def declare_warning(error, isInteractive)
+  if isInteractive
+    puts "An error has occured, continuing operation."
+    puts error
+  else
+    puts "An error has occured, continuing operation."
+    puts error
+  end
+end
